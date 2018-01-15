74dec19a7929796861c2b550fd2ba8d2080ca93c
Michael Molisani <molisani@seas.upenn.edu>
2018-01-15 13:42:00 -0500

    Added lib to npm package

--- dist/savagedom.js
+++ dist/savagedom.js
@@ -1,5 +1,5 @@
 /*
- * SavageDOM 1.0.0-alpha.18 (https://github.com/molisani/SavageDOM) 
+ * SavageDOM 1.0.0-alpha.19 (https://github.com/molisani/SavageDOM) 
  * Copyright 2018 Michael Molisani
  * Licensed under LGPL-3.0 (https://github.com/molisani/SavageDOM/blob/master/LICENSE)
  */
