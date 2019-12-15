043134ca9a0525c245b9252fba79e28adfc09ab3
Michael Molisani <molisani@users.noreply.github.com>
2019-12-15 14:51:01 -0500

    refactor: move extensions to separate file (#70)

--- src/element.ts
+++ src/element.ts
@@ -1,11 +1,8 @@
-import { computeActualEffectTiming, SVGAnimation, SVGEffectTiming, SVGRenderer } from "./animations";
-import { getAttribute, SavageDOMAttributes, setAttribute } from "./attributes";
-import { getInterpolator, SVGPropertyInterpolators } from "./interpolators";
+import { SVGAnimation, SVGEffectTiming } from "./animations";
+import { SavageDOMAttributes } from "./attributes";
+import { SVGElementTagName, SVGElementTagNames, XMLNS } from "./constants";
+import { add, createChild, SavageDOMElementPrototype } from "./extensions";
 import { SVGElementStyleTagNameMap, SVGStyleType } from "./style";
-
-type SVGElementTagName = keyof SVGElementTagNameMap;
-
-const SVGElementTagNames = new Set<SVGElementTagName>(["a","circle","clipPath","defs","desc","ellipse","feBlend","feColorMatrix","feComponentTransfer","feComposite","feConvolveMatrix","feDiffuseLighting","feDisplacementMap","feDistantLight","feFlood","feFuncA","feFuncB","feFuncG","feFuncR","feGaussianBlur","feImage","feMerge","feMergeNode","feMorphology","feOffset","fePointLight","feSpecularLighting","feSpotLight","feTile","feTurbulence","filter","foreignObject","g","image","line","linearGradient","marker","mask","metadata","path","pattern","polygon","polyline","radialGradient","rect","script","stop","style","svg","switch","symbol","text","textPath","title","tspan","use","view"]);
 
 interface AttributeAccessor<PROPS> {
   get<KEY extends keyof PROPS>(key: NonNullable<KEY>): PROPS[KEY];
@@ -27,8 +24,6 @@ type SavageDOMElementConstructor<ELEMENT extends SVGElement = SVGElement> = (pro
 
 type ElementWrapper = <ELEMENT extends SVGElement = SVGElement>(element: ELEMENT) => SavageDOMElement<ELEMENT>;
 
-type ElementSearchConstructor = new(wrapFn: ElementWrapper, parent: SVGElement) => ElementCollectionsByTagName;
-
 type ElementCreator = {
   [TAG in SVGElementTagName]: SavageDOMElementConstructor<SVGElementTagNameMap[TAG]>;
 };
@@ -37,86 +32,11 @@ interface SVGParent {
   add: ElementCreator & ((element: SVGElement, prefix?: boolean) => void);
   rm(element: SVGElement): void;
   inject(doc: Document): SavageDOMElement<SVGGElement>;
-  readonly sub: ArrayLike<SavageDOMElement>;
-  readonly subByTag: ElementCollectionsByTagName;
+  sub(): ArrayLike<SavageDOMElement>;
+  subByTag<TAG extends SVGElementTagName>(tagName: TAG): ArrayLike<SavageDOMElement<SVGElementTagNameMap[TAG], SVGElementStyleTagNameMap[TAG]>>;
 }
 
 export type SavageDOMElement<ELEMENT extends SVGElement = SVGElement, STYLE = SVGStyleType<ELEMENT>> = Omit<ELEMENT, "__SavageDOMStyleProperties"> & AttributeAccessor<SavageDOMAttributes<ELEMENT> & STYLE> & SVGEventEmitter & SVGParent;
-
-
-function _assignProperties<ELEMENT extends SVGElement>(element: SavageDOMElement<ELEMENT>, props: Partial<SavageDOMAttributes<ELEMENT>>) {
-  for (const [key, value] of Object.entries(props)) {
-    element.set(key as any, value as any);
-  }
-}
-
-function _createChild(wrapFn: ElementWrapper, parent: SVGElement, tagName: SVGElementTagName, props?: SavageDOMAttributes): SavageDOMElement {
-  const child = document.createElementNS("http://www.w3.org/2000/svg", tagName);
-  parent.append(child);
-  const wrapped = wrapFn(child);
-  if (props) {
-    _assignProperties(wrapped, props);
-  }
-  return wrapped;
-}
-
-interface SVGDocument extends Document {
-  children: HTMLCollection & [SVGSVGElement];
-}
-
-function _isSVGDocument(doc: Document): doc is SVGDocument {
-  return doc.children.length === 1 && doc.children[0] instanceof SVGSVGElement;
-}
-
-function _importDocument(svg: SVGElement, foreignDocument: Document): SVGGElement {
-  const localRoot = svg.ownerSVGElement;
-  const localDocument = svg.ownerDocument;
-  if (!localRoot || !localDocument) {
-    throw new Error("Target element must live inside a <svg>");
-  }
-  if (!_isSVGDocument(foreignDocument)) {
-    throw new Error("Can only inject SVG documents (with <svg> as root node)");
-  }
-  const foreignRoot = foreignDocument.children[0];
-  for (const foreignDefs of Array.from(foreignRoot.getElementsByTagNameNS("http://www.w3.org/2000/svg", "defs"))) {
-    foreignRoot.removeChild(foreignDefs);
-    localRoot.append(foreignDefs);
-  }
-  const g = localDocument.createElementNS("http://www.w3.org/2000/svg", "g");
-  for (const child of Array.from(foreignRoot.children)) {
-    const importedChild = localDocument.importNode(child as SVGElement, true);
-    g.append(importedChild);
-  }
-  return g;
-}
-
-function _mapCollection(collection: HTMLCollection, map: (element: SVGElement) => SavageDOMElement): ArrayLike<SavageDOMElement> {
-  return new Proxy(collection, {
-    get: (_obj: any, key: keyof HTMLCollection) => {
-      if (key === "length") {
-        return collection.length;
-      }
-      if (key in collection) {
-        return map(collection[key] as any);
-      }
-    },
-  });
-}
-
-const _ElementSearch = (() => {
-  class ElementSearch {
-    constructor(public wrapFn: ElementWrapper, public parent: SVGElement) {}
-  }
-  for (const tagName of SVGElementTagNames) {
-    Object.defineProperty(ElementSearch.prototype, tagName, {
-      get: function(this: ElementSearch) {
-        const collection = this.parent.getElementsByTagNameNS("http://www.w3.org/2000/svg", tagName);
-        return _mapCollection(collection, this.wrapFn);
-      },
-    });
-  }
-  return ElementSearch as any as ElementSearchConstructor;
-})();
 
 const __symbol = Symbol("SavageDOMElement");
 
@@ -131,51 +51,23 @@ function _wrap<ELEMENT extends SVGElement>(element: ELEMENT): SavageDOMElement<E
     return element;
   }
   Object.defineProperty(element, __symbol, { value: null });
-  function _add(child: SVGElement, prefix: boolean = false) {
-    (prefix) ? element.prepend(child) : element.append(child);
-  }
-  Object.assign(element, {
-    get: (key: string) => {
-      return getAttribute(element, key as any)[1];
-    },
-    set: setAttribute.bind(void 0, element),
-    animateTo: (timing: SVGEffectTiming, keyOrProps: any, value?: any) => {
-      const computedTiming = computeActualEffectTiming(timing);
-      const interpolators: SVGPropertyInterpolators<any> = {};
-      if (typeof keyOrProps === "string") {
-        interpolators[keyOrProps] = getInterpolator(element, keyOrProps as any, value);
-      } else {
-        for (const [key, val] of Object.entries(keyOrProps)) {
-          interpolators[key] = getInterpolator(element, key as any, val);
-        }
-      }
-      const renderer = SVGRenderer.getFromWindow(window);
-      return renderer.enqueue(computedTiming, element as any, interpolators);
-    },
-    once: (type: string) => new Promise((res) => element.addEventListener(type, res, { once: true })),
-    add: new Proxy(_add, {
+  Object.assign(element, SavageDOMElementPrototype);
+  Object.defineProperty(element, "add", {
+    value: new Proxy(add, {
       get: (_obj, tagName: SVGElementTagName) => {
-        return _createChild.bind(void 0, _wrap, element, tagName);
-      },
+        return createChild.bind(void 0, element, tagName);
+      }
     }),
-    rm: (child: SVGElement) => element.removeChild(child),
-    inject: (doc: Document) => {
-      const imported = _importDocument(element, doc);
-      _add(imported);
-      return _wrap(imported);
-    },
   });
-  const sub = _mapCollection(element.children, _wrap);
-  Object.defineProperty(element, "sub", { get: () => sub });
-  const subByTag = new _ElementSearch(_wrap, element);
-  Object.defineProperty(element, "subByTag", { get: () => subByTag });
   return element as any as SavageDOMElement<ELEMENT>;
 }
 
-function _wrapWithProps<ELEMENT extends SVGElement>(element: ELEMENT, props?: Partial<SavageDOMAttributes<ELEMENT> & SVGStyleType<ELEMENT>>): SavageDOMElement<ELEMENT> {
+export function wrap<ELEMENT extends SVGElement>(element: ELEMENT, props?: Partial<SavageDOMAttributes<ELEMENT> & SVGStyleType<ELEMENT>>): SavageDOMElement<ELEMENT> {
   const wrappedElement = _wrap(element);
   if (props) {
-    _assignProperties(wrappedElement, props);
+    for (const [key, value] of Object.entries(props)) {
+      wrappedElement.set(key as any, value as any);
+    }
   }
   return wrappedElement;
 }
@@ -183,9 +75,9 @@ function _wrapWithProps<ELEMENT extends SVGElement>(element: ELEMENT, props?: Pa
 const _creator = (() => {
   const creator = Object.create(null);
   for (const tagName of SVGElementTagNames) {
-    creator[tagName] = (properties: any) => _wrapWithProps(document.createElementNS("http://www.w3.org/2000/svg", tagName), properties);
+    creator[tagName] = (properties: any) => wrap(document.createElementNS(XMLNS.SVG, tagName), properties);
   }
   return creator as ElementCreator;
 })();
 
-export const element = Object.assign(_wrapWithProps, _creator);
+export const element = Object.assign(wrap, _creator);
---
+++ src/extensions.ts
@@ -0,0 +1,108 @@
+import { computeActualEffectTiming, SVGEffectTiming, SVGRenderer } from "./animations";
+import { getAttribute, SavageDOMAttributes, setAttribute } from "./attributes";
+import { SVGElementTagName, XMLNS } from "./constants";
+import { SavageDOMElement, wrap } from "./element";
+import { getInterpolator, SVGPropertyInterpolators } from "./interpolators";
+
+export function add(element: SavageDOMElement, child: SVGElement, prefix: boolean = false) {
+  (prefix) ? element.prepend(child) : element.append(child);
+}
+
+function _assignProperties<ELEMENT extends SVGElement>(element: SavageDOMElement<ELEMENT>, props: Partial<SavageDOMAttributes<ELEMENT>>) {
+  for (const [key, value] of Object.entries(props)) {
+    element.set(key as any, value as any);
+  }
+}
+
+export function createChild(parent: SVGElement, tagName: SVGElementTagName, props?: SavageDOMAttributes): SavageDOMElement {
+  const child = document.createElementNS(XMLNS.SVG, tagName);
+  parent.append(child);
+  const wrapped = wrap(child);
+  if (props) {
+    _assignProperties(wrapped, props);
+  }
+  return wrapped;
+}
+
+interface SVGDocument extends Document {
+  children: HTMLCollection & [SVGSVGElement];
+}
+
+function _isSVGDocument(doc: Document): doc is SVGDocument {
+  return doc.children.length === 1 && doc.children[0] instanceof SVGSVGElement;
+}
+
+function _importDocument(svg: SVGElement, foreignDocument: Document): SVGGElement {
+  const localRoot = svg.ownerSVGElement;
+  const localDocument = svg.ownerDocument;
+  if (!localRoot || !localDocument) {
+    throw new Error("Target element must live inside a <svg>");
+  }
+  if (!_isSVGDocument(foreignDocument)) {
+    throw new Error("Can only inject SVG documents (with <svg> as root node)");
+  }
+  const foreignRoot = foreignDocument.children[0];
+  for (const foreignDefs of Array.from(foreignRoot.getElementsByTagNameNS(XMLNS.SVG, "defs"))) {
+    foreignRoot.removeChild(foreignDefs);
+    localRoot.append(foreignDefs);
+  }
+  const g = localDocument.createElementNS(XMLNS.SVG, "g");
+  for (const child of Array.from(foreignRoot.children)) {
+    const importedChild = localDocument.importNode(child as SVGElement, true);
+    g.append(importedChild);
+  }
+  return g;
+}
+
+function _mapCollection(collection: HTMLCollection, map: (element: SVGElement) => SavageDOMElement): ArrayLike<SavageDOMElement> {
+  return new Proxy(collection, {
+    get: (_obj: any, key: keyof HTMLCollection) => {
+      if (key === "length") {
+        return collection.length;
+      }
+      if (key in collection) {
+        return map(collection[key] as any);
+      }
+    },
+  });
+}
+
+export const SavageDOMElementPrototype = {
+  get: function(this: SavageDOMElement, key: string): any {
+    return getAttribute(this, key as any)[1];
+  },
+  set: function(this: SavageDOMElement, ...args: [string, any] | [object]) {
+    (args.length === 1) ? setAttribute(this, args[0]) : setAttribute(this, args[0], args[1]);
+  },
+  animateTo: function(this: SavageDOMElement, timing: SVGEffectTiming, keyOrProps: any, value?: any) {
+    const computedTiming = computeActualEffectTiming(timing);
+    const interpolators: SVGPropertyInterpolators<any> = {};
+    if (typeof keyOrProps === "string") {
+      interpolators[keyOrProps] = getInterpolator(this, keyOrProps as any, value);
+    } else {
+      for (const [key, val] of Object.entries(keyOrProps)) {
+        interpolators[key] = getInterpolator(this, key as any, val);
+      }
+    }
+    const renderer = SVGRenderer.getFromWindow(window);
+    return renderer.enqueue(computedTiming, this as any, interpolators);
+  },
+  once: function(this: SavageDOMElement, type: string) {
+    return new Promise((res) => this.addEventListener(type, res, { once: true }));
+  },
+  rm: function(this: SavageDOMElement, child: SVGElement) {
+    this.removeChild(child)
+  },
+  inject: function(this: SavageDOMElement, doc: Document): any {
+    const imported = _importDocument(this, doc);
+    add(this, imported);
+    return wrap(imported);
+  },
+  sub: function(this: SavageDOMElement): any {
+    return _mapCollection(this.children, wrap);
+  },
+  subByTag: function(this: SavageDOMElement, tagName: SVGElementTagName): any {
+    const collection = this.getElementsByTagNameNS(XMLNS.SVG, tagName);
+    return _mapCollection(collection, wrap);
+  },
+} as const;
