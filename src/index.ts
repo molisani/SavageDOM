1baf623047d5139b1b97a23688fb01602ff316cc
Michael Molisani <git@molisani.us>
2020-01-04 20:51:46 -0500

    Export types from animation module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import { element, SavageDOMElement } from "./element";
+export { SVGAnimation, SVGEffectTiming } from "./animations";
 export * from "./colors";
 export { transplant } from "./extensions";
 export { FontFamily } from "./style";
